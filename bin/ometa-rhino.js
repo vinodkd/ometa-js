ad2aac1e8d36abd053e99e4fed17900161748935
vinodkd <vinod@vinodkd.org>
2013-03-13 10:30:49 +0530

    Finally got the single-stage ometa-js launcher to work. This commit represents the working, but not cleaned up version of the code. The reason I'm checking this in is that it contains a lot of work-in-progress info in the comments, debug prints and extra variables.
    Problems fixed:
    1. the ometa launcher now doesnt assume the BSParser or BSCompiler - those can be passed in as params
    2. It also doesnt do both stages together. This allows composing parse/compile/interpret steps independently. so projects like betterscript can actually use ometa-js as a true external dependency.
    	getting this to work was the big achievement. This required adding in crockford's json library
    
    Pending:
    - Clean up everything that's not required for this to work.
    - determining the type of source - the source|tree param for ometa - is kludgey. see if createInstance can be used instead.

--- bin/ometa-rhino.js
+++ bin/ometa-rhino.js
@@ -9,6 +9,7 @@ load(ometalibs + "bs-js-compiler.js");
 load(ometalibs + "bs-ometa-compiler.js");
 load(ometalibs + "bs-ometa-optimizer.js");
 load(ometalibs + "bs-ometa-js-compiler.js");
+load(ometalibs + "ext/json2.js");
 
 matchFailed = function (grammar, errorPos) {
   var lines = grammar.input.lst.split('\n');
@@ -42,12 +43,57 @@ translateCode = function(s) {
   tree = BSOMetaJSParser.matchAll(s, "topLevel", undefined, matchFailed)
   return BSOMetaJSTranslator.match(tree, "trans", undefined, matchFailed)
 }
+
+/* 
+This whoel section is under construction
+The idea was to take omet-rhino.js from betterscript and convert it into a generic one-step processor:
+parse would be one step, compile would be another; which is currently combined in betterscript/ometa-rhino.
+however, when i split the two, the parse seems to work fine, but the compile refuses to accept the output of the parse step
+i've narrowed it down to this: if both the parse and compile are in the same session, the compile works.
+splitting them up requires writing the output to file and reading it back. something is going on there.
+
+when i figure it out, all hardcoded refs shoudl be removed, as should all the srctype elvises.
+even better: figure out how to use createInstance.
+
+idea: eval the source when its an object.
+its almost working but there's issues in the eval 
+FINALLY GOT IT WORKING BY MANUALLY CHANGING ONE.BSC: NEED TO SAVE THE OBJECT AS A REPARSEABLE OBJECT.
+BEST BET: USE JSON
+*/
 var grammar  = readFile(arguments[1]);
+var compiler = readFile("/Users/vinodkd/projects/betterscript/bsc.ometa");
 var rule     = arguments[2];
-var pgm      = readFile(arguments[3]);
+var srcType  = arguments[3];
+var srcTxt   = readFile(arguments[4]);
+// alert(srcTxt);
 
-var langtoolchain = translateCode(grammar);
-
+var langtoolchain = translateCode(grammar + "\n" + compiler);
 eval(langtoolchain);
-var tree = BSParser.matchAll(pgm,rule);
-alert(tree);
+
+// really kludge-y way of getting the parser's name.
+// todo: figure out how to use omet-base.js's createInstance instead
+// var ometaObj = langtoolchain.substr(0,langtoolchain.indexOf("="));
+//alert(langtoolchain);
+//alert(ometaObj);
+var ometaObj = srcType == "source" ? "BSParser" : "BSCompiler";
+var fnToCall = srcType == "source" ? "matchAll" : "match";
+
+var src;
+(srcType == "source") ? src = srcTxt : eval('src = ' + srcTxt +';');
+
+// alert(fnToCall);
+//alert(src);
+//alert("tree = " + ometaObj + "." + fnToCall + "(src,rule,undefined,matchFailed)");
+eval("tree = " + ometaObj + "." + fnToCall + "(src,rule);"); //,undefined,matchFailed)");
+//var tree = BSCompiler.match(src,rule,undefined,matchFailed);
+alert((srcType == "source") ? JSON.stringify(tree) : tree );
+
+// var ast = BSParser.matchAll(src,"compilationUnit");
+// alert(ast);
+// var bsc = readFile("/Users/vinodkd/projects/betterscript/one.bsc");
+// alert("printing file output");
+// alert(bsc);
+// alert("ast done. now compiling\n");
+
+// var code = BSCompiler.match(ast, "ast");
+// alert(code);
